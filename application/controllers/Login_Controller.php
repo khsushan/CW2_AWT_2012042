a6d30d930930601819d20e88c4daa44a032f2364
khsushan <khsushan@gmail.com>
2016-01-07 23:22:57 +0530

    add login api

--- application/controllers/Login_Controller.php
+++ application/controllers/Login_Controller.php
@@ -11,32 +11,32 @@
  *
  * @author Ushan
  */
-class Login_Controller extends CI_Controller {
+class Login_Controller extends CI_Controller
+{
 
-    //put your code here
-//    function index(){
-//        $this->load->model('Vacancy');
-//        $vacancy = new Vacancy();
-//        $array = $vacancy->getVacancyByUser(1);
-//        $data["vacancies"] = $array;
-//        $this->load->view('organizerview',$data);   
-//    }
-    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session');
+    }
+
     /**
      * This method will handle the signup login process
      */
-    function login() {
+    function login()
+    {
         $this->load->model('User_Model');
         $user_model = new User_Model();
         $json_data = json_decode(file_get_contents('php://input'));
         $email = $json_data->{'email'};
         $password = $json_data->{'password'};
         $user_details = $user_model->getUserDetials($email, $password);
-        if($user_details != NULL){
+        if ($user_details != NULL) {
             $user_details[0]["error"] = false;
+            $this->session->set_userdata('user', $user_details);
             echo json_encode($user_details[0]);
-        }  else {
-            $data = array("error"=>true,"message"=>"invalid username or password");
+        } else {
+            $data = array("error" => true, "message" => "invalid username or password");
             echo json_encode($data);
         }
     }
