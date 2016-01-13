239ef8fedec09f84bc4fa1fa7976349496650a50
khsushan <khsushan@gmail.com>
2016-01-13 21:15:15 +0530

    adding  add attempt method

--- application/models/Attemp_Model.php
+++ application/models/Attemp_Model.php
@@ -11,38 +11,52 @@
  *
  * @author Ushan
  */
-class Attemp_Model {
-    
-    public function __construct() {
+class Attemp_Model extends  CI_Model
+{
+
+    public function __construct()
+    {
         $this->load->database();
     }
-    
+
     /*
      * This method will add a new attemp to the database 
      * @param userid
      * @param score
      *         
      * */
-    public function addAttemp($user_id,$score){
-         $data = array(
-                'user_id' => $user_id,
-                'score' => $score
-         );
-         $this->db->insert('attemp', $data);
+    public function addAttemp($user_id, $result)
+    {
+        $data = array(
+            'user_id' => $user_id,
+            'score' => $result["score"],
+            'time'=>"00.00"
+        );
+        $this->db->insert('attemp', $data);
+        $last_id = $this->db->insert_id();
+        $feedbackArray = $result["feedbaks"];
+        for($i = 0; $i <count($feedbackArray);$i++){
+            $data = array(
+                'question_id' => $feedbackArray[$i]["question_id"],
+                'attempt_id' => $last_id,
+                'isCorrect' => ($feedbackArray[$i]["correct_answer"] == $feedbackArray[$i]["user_answer"])
+            );
+            $this->db->insert('question_attempt', $data);
+        }
     }
-    
+
     /*
      * This method will return attemp detils regarding to give user id 
      * @param userid
      *         
      * */
-    public function getAttempByUserID($user_id){
+    public function getAttempByUserID($user_id)
+    {
         $this->db->where('user_id', $user_id);
         $query = $this->db->get('attemp');
         $attmps = array_values($query->result_array());
         return $attmps;
     }
-    
-    
-    
+
+
 }
