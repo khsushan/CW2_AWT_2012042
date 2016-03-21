34eb96f065af17e36df25f53173215b4b4380ccb
khsushan <khsushan@gmail.com>
2016-03-21 00:13:19 -0700

    add new UI changes and logout for noramal users

--- application/views/question_view.php
+++ application/views/question_view.php
@@ -1,43 +1,94 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="en">
+
 <head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="Sergey Pozhilov (GetTemplate.com)">
+    <meta name="author" content="">
 
-    <title>Check your knowledge and have fun</title>
+    <title>Creative - Start Bootstrap Theme</title>
 
-    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/gt_favicon.png">
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css" type="text/css">
 
-    <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+    <!-- Custom Fonts -->
+    <link
+        href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+        rel='stylesheet' type='text/css'>
+    <link
+        href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/font-awesome/css/font-awesome.min.css" type="text/css">
 
-    <!-- Custom styles for our template -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-theme.css" media="screen">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/main.css">
+    <!-- Plugin CSS -->
+    <!--    <link rel="stylesheet" href="-->
+    <?php //echo base_url(); ?><!--assets/css/animate.min.css" type="text/css">-->
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/creative.css" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
 
-<body class="home" onload="getSessionTime()">
-<!-- Fixed navbar -->
-<div class="navbar navbar-inverse navbar-fixed-top headroom">
-    <div class="container">
+<body id="page-top" onload="getSessionTime()">
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/timehandle.js"></script>
+<script type="text/javascript">
+    function logout(){
+        $.post("http://localhost/CW2_AWT_2012042/user/logout",{},
+            function (data) {
+                if(data == 'error') {
+                    alert("please log in first!");
+                }else {
+                    window.location.href = "http://localhost/CW2_AWT_2012042/";
+                }
+            });
+    }
+</script>
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <!-- Button for smallest screens -->
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span
-                    class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></button>
-            <a class="navbar-brand" href=""><img src="<?php echo base_url(); ?>assets/images/quiz.png"> QUIZ TIME</a>
-        </div>
-    </div>
-</div>
-<!-- /.navbar -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">QUIZ TIME</a>
 
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <!--<script type="text/template" id="navigate-template">-->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div id="navigate">
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="btn" href="<?php echo base_url(); ?>" >Home</a>
+                    </li>
+                    <li>
+                        <a class="btn" href="javascript:logout()">Logout</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+        <!--</script>-->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
 <!-- Header -->
 <header id="head">
     <div class="container">
@@ -50,12 +101,12 @@ and open the template in the editor.
                 <div class="btn-group">
                     <div class="col-lg-6"><h5>Category : </h5></div>
                     <div class="col-lg-6">
-                    <select id="category_name" name="category_name" class="dropdown btn-group">
-                        <?php for($i =0 ; $i < count($category); $i++){ ?>
+                        <select id="category_name" name="category_name" class="dropdown btn-group">
+                            <?php for ($i = 0; $i < count($category); $i++) { ?>
                                 <option value="<?php echo $category[$i]["category_name"] ?>">
                                     <?php echo $category[$i]["category_name"] ?></option>
-                        <?php } ?>
-                    </select>
+                            <?php } ?>
+                        </select>
                     </div>
                 </div>
                 <br>
@@ -72,10 +123,7 @@ and open the template in the editor.
         ?>
         <!-- Question view -->
         <form name="userinput" action="next" method="post">
-            <div id="remain_question_timer">
-                <div class="col-lg-6"><h3>QUESTION <?php echo $question[0]["question_number"] ?>/10</h3></div>
-                <div class="col-lg-6"><h3 align="left" id="timer">00.00</h3></div>
-            </div>
+            <br><br><br>
             <div class="col-lg-12">
                 <!-- Question View -->
                 <h3><?php echo $question[0]["question_number"] . " . " . $question[0]["question_value"] ?></h3>
@@ -100,37 +148,45 @@ and open the template in the editor.
                         Next
                     </button>
                 </p>
+            </div>
+            <div id="remain_question_timer">
+                <div class="col-lg-6"><h3>QUESTION <?php echo $question[0]["question_number"] ?>/10</h3></div>
+                <div class="col-lg-6"><h3 align="left" id="timer">00.00</h3></div>
+            </div>
         </form>
     </div>
     <!-- End Question view -->
     <?php
     } else if (isset($results)) { ?>
-        <!--Score View Page -->
-        <form  onload="drawChart()" name="userinput" action="<?php echo base_url(); ?>" method="post">
 
+        <!--Score View Page -->
+        <form onload="drawChart()" name="userinput" action="<?php echo base_url(); ?>" method="post">
+            <br><br><br>
             <h3>Hi <?php echo $user ?> ,Your Score for this attempt is <?php echo $results["score"] ?>%</h3>
-                <?php
-                $total_score =0 ;
-                for ($i = 0; $i < count($attempts); $i++) {
+            <?php
+            $total_score = 0;
+            for ($i = 0; $i < count($attempts); $i++) {
 
                 ?>
                 <?php $total_score += $attempts[$i]["score"];
-                } ?>
-            <h3>Your Average is : <?php echo number_format((float)($total_score/count($attempts)), 2, '.', ''); ?> %</h3>
+            } ?>
+            <h3>Your Average is : <?php echo number_format((float)($total_score / count($attempts)), 2, '.', ''); ?>
+                %</h3>
             <p>
                 <button type="submit" class="btn btn-default btn-lg"
-                        id="try_again_btn" name="try_again_btn" >
+                        id="try_again_btn" name="try_again_btn">
                     Try Again
                 </button>
                 <button type="button" class="btn btn-default btn-lg"
-                        id="view_chart" name="view_chart" onclick="drawChart()" >
+                        id="view_chart" name="view_chart" onclick="drawChart()">
                     View Progress
                 </button>
             </p>
             <!--                <h1 align="Right" id="timeText">Total time for quiz :</h1>-->
 
             <h3 align="Right" id="timer">00.00</h3>
-            <div id="chart_div"><div>
+            <div id="chart_div">
+                <div>
         </form>
     <?php } ?>
     <!--end of score view-->
@@ -141,44 +197,58 @@ and open the template in the editor.
 </header>
 <!-- /Header -->
 
-<footer id="footer" class="top-space navbar-fixed-bottom">
-    <div class="footer2">
-        <div class="container">
-            <div class="row">
-
-                <div class="col-md-6 widget">
-                    <div class="widget-body">
-                        <p class="simplenav">
-                            <a href="#">Home</a> |
-                            <a href="about.html">About</a> |
-                        </p>
-                    </div>
-                </div>
-
-                <div class="col-md-6 widget">
-                    <div class="widget-body">
-                        <p class="text-right">
-                            Copyright &copy; 2015, Sachith Ushan
-                        </p>
-                    </div>
-                </div>
-
+<!-contact view ->
+<div id="contact" class="bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 text-center">
+                <h2 class="section-heading">Let's Get In Touch!</h2>
+                <hr class="primary">
+                <p>Ready to start your next quiz with me? That's great! Give us a call or send us an email and we will
+                    get back to you as soon as possible!</p>
             </div>
-            <!-- /row of widgets -->
+            <div class="col-lg-4 col-lg-offset-2 text-center">
+                <i class="fa fa-phone fa-3x wow bounceIn"></i>
+
+                <p>+94718853336</p>
+            </div>
+            <div class="col-lg-4 text-center">
+                <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+
+                <p><a href="mailto:your-email@your-domain.com">khsushan@gmail.com</a></p>
+            </div>
         </div>
     </div>
-</footer>
+</div>
+
+
+<!-- jQuery -->
+<script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="<?php echo base_url(); ?>assets/js/jquery.easing.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.fittext.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/wow.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="<?php echo base_url(); ?>assets/js/creative.js"></script>
+
 
 <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/timehandle.js"></script>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/progress_chart.js">
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/headroom.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/progress_chart.js"></script>
+<script
+    type="text/javascript"
+    src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script
+    src="<?php echo base_url(); ?>assets/js/headroom.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/headroom.min.js"></script>
 
 
 </body>
-</html> 
+
+</html>
